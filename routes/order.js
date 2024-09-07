62674743f8d6f2fad8c322b0e8fa729d732df664
eastwdTW <vincent71334@gmail.com>
2024-09-07 12:19:54 +0800

    test

--- routes/order.js
+++ routes/order.js
@@ -1,29 +1,29 @@
 import express from 'express'
-import "../db/orders.json"
+// import "../db/orders.json"
 
 const router = express.Router()
 
-router.post('/start', (_, res) => {
-	genNumber()
-	res.json({msg: true})
-})
-router.get('/guess', (req, res) => {
-	const number = getNumber()
-	const guessed = roughScale(req.query.number, 10)
+// router.post('/start', (_, res) => {
+// 	genNumber()
+// 	res.json({msg: true})
+// })
+// router.get('/guess', (req, res) => {
+// 	const number = getNumber()
+// 	const guessed = roughScale(req.query.number, 10)
 
-	if(!guessed || guessed < 1 || guessed > 100) {
-		res.status(406).send({msg: 'Not a legal number.'})
-	}else if(number === guessed){
-		res.json({msg: 'Equal'})
-	}else if(number > guessed){
-		res.json({msg: 'Bigger'})
-	}else{
-		res.json({msg: 'Smaller'})
-	}
-})
-router.post('/restart', (_, res) => {
-	genNumber()
-	res.json({msg: false})
-})
+// 	if(!guessed || guessed < 1 || guessed > 100) {
+// 		res.status(406).send({msg: 'Not a legal number.'})
+// 	}else if(number === guessed){
+// 		res.json({msg: 'Equal'})
+// 	}else if(number > guessed){
+// 		res.json({msg: 'Bigger'})
+// 	}else{
+// 		res.json({msg: 'Smaller'})
+// 	}
+// })
+// router.post('/restart', (_, res) => {
+// 	genNumber()
+// 	res.json({msg: false})
+// })
 
 export default router
