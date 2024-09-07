148b349305c94d8a1b2be41ad0e623e25fdebd89
eastwdTW <vincent71334@gmail.com>
2024-09-07 12:55:32 +0800

    template 3.0

--- route.js
+++ route.js
@@ -1,9 +1,48 @@
 import express from 'express'
 import fs from 'fs';
 import path from 'path';
-import "./db/drivers.json"
 
 const router = express.Router()
+
+router.get('/test', (req, res) => {
+	const jsonFilePath = path.join(__dirname, './db/users.json');
+
+	var Data;
+
+	fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+		Data = Json.parse(data)
+	});
+
+	console.log
+
+	return res.status(200)
+});
+
+router.post('/login', (req, res) => {
+	const jsonFilePath = path.join(__dirname, '../db/users.json');
+
+	fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+		// if (err) {
+		// 	return res.status(500).json({ message: 'Error reading file', error: err });
+		// }
+
+		// try {
+		// 	const drivers = JSON.parse(data);
+		// 	res.json(drivers);
+		// } catch (parseError) {
+		// 	res.status(500).json({ message: 'Error parsing JSON', error: parseError });
+		// }
+		console.log(data)
+	});
+});
+
+router.post('/regist', (req, res) => {
+
+});
+
+router.post('/forget-password', (req, res) => {
+
+});
 
 router.get('/drivers', (req, res) => {
 	const jsonFilePath = path.join(__dirname, '../db/drivers.json');
