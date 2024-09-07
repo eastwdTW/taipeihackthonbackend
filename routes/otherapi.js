7ebb524bfd55b92651860a84fbe1a95aa6c9cba3
erictsaii <erictsai90@gmail.com>
2024-09-07 17:16:12 +0800

    update driver data

--- routes/otherapi.js
+++ routes/otherapi.js
@@ -27,10 +27,44 @@ router.post('/reserve', urlencodedParser, (req, res) => {
 
 });
 
-router.get('available/car', (req, res) => {
-	const {from, to, startDate, carType} = req.query;
+router.get('/available/car', (req, res) => {
+	const ordersFilePath = path.join(__dirname, '../db/orders.json');
+	const { from, to, startDate, carType } = req.query;
 
-	
+	fs.readFile(jsonFilePath, 'utf8', (err, data) => {
+		if (err) {
+			return res.status(500).json({ message: 'Error reading orders file', status: false });
+		}
+
+		let orders = [];
+		if (data) {
+			orders = JSON.parse(data).orders;
+		}
+		const fromDate = new Date(from);
+		const toDate = new Date(to);
+
+		const conflictOrders = orders.filter(order => {
+			const orderFromDate = new Date(order.from);
+			const orderToDate = new Date(order.to);
+			return (fromDate >= orderFromDate) && (fromDate <= orderToDate) || (toDate >= orderFromDate) && (toDate <= orderToDate);
+		});
+
+
+
+
+	});
+
+	const availableDriver = driversData.drivers.find(driver => driver.online && driver.carType === carType);
+
+	if (!availableDriver) {
+		return res.status(404).json({ message: 'No available cars found', status: false });
+	}
+
+	res.json({
+		message: 'Available car found',
+		driver: availableDriver,
+		status: true
+	});
 });
 
 export default router
